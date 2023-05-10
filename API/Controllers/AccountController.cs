f6b4259976ba8d41258d3285327274bc8c52914a
ryumartinez <ryueduardomartinez@outlook.es>
2023-05-10 15:10:08 -0400

    Added Login Endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,4 +36,20 @@ public class AccountController : BaseApiController
     {
         return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        if (user == null) return Unauthorized("Invalid username");
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac. ComputeHash (Encoding.UTF8.GetBytes(loginDto. Password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+        }
+
+        return user;
+    }
+    
 }
